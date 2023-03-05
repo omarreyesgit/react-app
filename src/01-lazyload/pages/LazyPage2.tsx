49c1bfb1ad5d310a9870473fb71cd9d015180814
omar reyes <arturreyes90@gmail.com>
2023-03-05 22:45:43 +0100

    Adding lazy routes

--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -1,3 +1,4 @@
 export const LazyPage2 = () => {
   return <div>LazyPage2</div>
 }
+export default LazyPage2
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -1,3 +1,4 @@
 export const LazyPage3 = () => {
   return <div>LazyPage3</div>
 }
+export default LazyPage3
